f52209b45cfa1317dddde723a6d7702d10da8c4d
Zihao Lin <216423917+ZihaoLin111@users.noreply.github.com>
2026-01-05 20:42:38 +0800

    feat(Task1): test with DIDN

---
+++ MTL/Task1/test_withDIDN.py
@@ -0,0 +1,113 @@
+from pickletools import optimize
+from numpy import test
+from sympy import im
+import torch
+import torch.nn as nn
+import torchvision
+from model import DUB, DIDN
+import tqdm
+from dataset import SIDD_Dataset
+from pytorch_msssim import ssim
+import datetime
+import os
+import matplotlib.pyplot as plt
+import random
+from PIL import Image
+
+model_path = './saved_models/DIDN_20251226_0343_ep20_loss0.0135_psnr33.67dB.pth'
+data_path = 'data/SIDD_Small_sRGB_Only'
+
+transform = torchvision.transforms.ToTensor()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+net = DIDN().to(device)
+criterion = nn.L1Loss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+def psnr(img1, img2):
+    mse = torch.mean((img1 - img2) ** 2)
+    if mse == 0:
+        return 100
+    return 20 * torch.log10(1.0 / torch.sqrt(mse))
+
+test_dataset = SIDD_Dataset(data_path, transform=transform, crop_size = 128, train=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+net.load_state_dict(torch.load(model_path, map_location=device))
+net.eval()
+initial_psnr = 0.0
+initial_ssim = 0.0
+total_psnr = 0.0
+total_ssim = 0.0
+count = 0
+
+with torch.no_grad():
+    for noisy_image, gt_image in test_loader:
+        noisy_image = noisy_image.to(device)
+        gt_image = gt_image.to(device)
+
+        for i in range(noisy_image.size(0)):
+            initial_psnr += psnr(noisy_image[i], gt_image[i]).item()
+            initial_ssim += ssim(noisy_image[i].unsqueeze(0), gt_image[i].unsqueeze(0), data_range=1.0).item() # SSIM要求4D输入
+
+
+        outputs = net(noisy_image)
+
+        # 限制在0-1之间
+        outputs = torch.clamp(outputs, 0.0, 1.0)
+        gt_image = torch.clamp(gt_image, 0.0, 1.0)
+
+        for i in range(outputs.size(0)):
+            total_psnr += psnr(outputs[i], gt_image[i]).item()
+            total_ssim += ssim(outputs[i].unsqueeze(0), gt_image[i].unsqueeze(0), data_range=1.0).item() # SSIM要求4D输入
+
+        count += outputs.size(0)
+    avg_psnr = total_psnr / count
+    avg_ssim = total_ssim / count
+    avg_initial_psnr = initial_psnr / count
+    avg_initial_ssim = initial_ssim / count
+    
+print(f'Iniatial: PSNR: {avg_initial_psnr:.4f}dB, SSIM: {avg_initial_ssim:.4f}  => Denoised: PSNR: {avg_psnr:.4f}dB, SSIM: {avg_ssim:.4f}')
+
+# 展示部分结果
+
+scene_file = os.path.join(data_path, 'Scene_Instances.txt')
+with open(scene_file, 'r') as f:
+    scene_instances = [line.strip() for line in f]
+Data_folder = os.path.join(data_path, 'Data')
+random_idx = random.randint(0, len(scene_instances) - 1)
+random_indice = scene_instances[random_idx]
+
+noisy_path = os.path.join(Data_folder, random_indice, 'NOISY_SRGB_010.PNG')
+gt_path = os.path.join(Data_folder, random_indice, 'GT_SRGB_010.PNG')
+
+noisy_image = Image.open(noisy_path).convert('RGB')
+gt_image = Image.open(gt_path).convert('RGB')
+        
+w, h = noisy_image.size
+
+i = (h - 256) // 2
+j = (w - 256) // 2
+noisy_image_copy = noisy_image
+noisy_image_copy = noisy_image_copy.crop((j, i, j + 256, i + 256))
+
+noisy_image = noisy_image.crop((j, i, j + 256, i + 256))
+
+noisy_image = transform(noisy_image).unsqueeze(0).to(device)
+
+outputs = net(noisy_image)
+            
+outputs = outputs.squeeze(0)
+outputs = torch.clamp(outputs, 0.0, 1.0) # 限制在0-1之间
+# Tensor to PIL Image
+to_pil = torchvision.transforms.ToPILImage()
+outputs = to_pil(outputs)
+
+# 对照组2 
+crop_gt = gt_image.crop((j, i, j + 256, i + 256))
+
+# 保存修复图和原图
+outputs.save('denoised_image.png')
+crop_gt.save('ground_truth_image.png')
+noisy_image_copy.save('noisy_image.png')
+
+print('图片已保存')
---
+++ MTL/model.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+
+class DUB(nn.Module): # Full DUB
+    def __init__(self, in_channels, out_channels):
+        super(DUB, self).__init__()
+        
+        # 下采样
+
+        # Level 0
+        self.con_00 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_00 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.con_01 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_01 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.downcon_0 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1) # 下采样
+            
+        # Level 1   
+        self.con_10 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_10 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.downcon_1 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1)# 下采样
+        
+        # Level 2
+        self.con_20 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_20 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.con_21 = nn.Conv2d(out_channels, out_channels*4, kernel_size=1, stride=1, padding=0)
+        self.up_2 = nn.PixelShuffle(upscale_factor=2) # 上采样
+
+        # 上采样
+        # Level 1
+        self.con_up10 = nn.Conv2d(out_channels*2, out_channels, kernel_size=1, stride=1, padding=0)
+        self.con_up11 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_up10 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.con_up12 = nn.Conv2d(out_channels, out_channels*4, kernel_size=1, stride=1, padding=0)
+        self.up_1 = nn.PixelShuffle(upscale_factor=2) # 上采样
+
+        # Level 0
+        self.con_up00 = nn.Conv2d(out_channels*2, out_channels, kernel_size=1, stride=1, padding=0)
+        self.con_up01 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_up00 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.con_up02 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_up01 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.con_up03 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        res0 = x
+
+        # 下采样
+        # Level 0
+        x0 = self.prelu_00(self.con_00(x))
+        x0 = self.prelu_01(self.con_01(x0)) + res0 # 后续拼接
+        
+        # Level 1
+        x1 = self.downcon_0(x0)
+        res1 = x1
+        x1 = self.prelu_10(self.con_10(x1)) + res1 # 后续拼接
+
+        # Level 2
+        x2 = self.downcon_1(x1)
+        res2 = x2
+        x2 = self.prelu_20(self.con_20(x2)) + res2
+        x2 = self.con_21(x2)
+
+        # 上采样
+        # Level 1
+        xu1 = self.up_2(x2)
+        xu1 = torch.cat((xu1, x1), dim=1)
+        xu1 = self.con_up10(xu1)
+        resu1 = xu1
+        xu1 = self.prelu_up10(self.con_up11(xu1)) + resu1
+        xu1 = self.con_up12(xu1)
+
+        # Level 0
+        xu0 = self.up_1(xu1)
+        xu0 = torch.cat((xu0, x0), dim=1)
+        xu0 = self.con_up00(xu0)
+        resu0 = xu0
+        xu0 = self.prelu_up00(self.con_up01(xu0))
+        xu0 = self.prelu_up01(self.con_up02(xu0)) + resu0
+        xu0 = self.con_up03(xu0)
+
+        return xu0 + res0
+
+
+
+class DIDN(nn.Module):
+    def __init__(self, in_channels=3, out_channels=128, num_dub=4):
+        super(DIDN, self).__init__()
+        self.entry = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.downcon = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1)
+        self.dub_blocks = nn.ModuleList([DUB(out_channels, out_channels) for _ in range(num_dub)])
+        self.con_10 = nn.Conv2d(out_channels * num_dub, out_channels, kernel_size=1, stride=1, padding=0)
+        self.con_11 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.prelu_10 = nn.PReLU(num_parameters=out_channels, init=0.25)
+        self.con_12 = nn.Conv2d(out_channels, out_channels * 4, kernel_size=1, stride=1, padding=0)
+        self.up = nn.PixelShuffle(upscale_factor=2)
+        self.exit = nn.Conv2d(out_channels, in_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        res1 = x
+        x = self.prelu(self.entry(x))
+        x = self.downcon(x)
+        recon = []
+        for dub in self.dub_blocks:
+            x = dub(x)
+            recon.append(x)
+        x = torch.cat(recon[::-1], dim=1)
+        x = self.con_10(x)
+        res2 = x
+        x = self.prelu_10(self.con_11(x)) + res2
+        x = self.con_12(x)
+        x = self.up(x)
+        x = self.exit(x)
+        return x + res1
+
+class SharedLayer(nn.Module):
+    def __init__(self, in_channels=3, out_channels=128):
+        super(SharedLayer, self).__init__()
+        
